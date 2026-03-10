9d0f296b2b9477440657c6bd8df281d93795fe32
Martinrinaudo <martinrinaudo03@gmail.com>
2026-03-10 11:01:40 -0300

    Dejar de rastrear environment.ts con credenciales sensibles

--- cuidar-frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  supabaseUrl: 'https://cvakzhgrnarlcvixhqzx.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2YWt6aGdybmFybGN2aXhocXp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMTA1NjIsImV4cCI6MjA1NTc4NjU2Mn0.hHcPDPBn4HBpkSAmRGaRavlOcaTSp7FiVBPN4HBjPDY'
-};
