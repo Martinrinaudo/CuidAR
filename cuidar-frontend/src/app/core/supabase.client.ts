4432466cf7b193ac26b1db9a95b877e1892770cd
Martinrinaudo <martinrinaudo03@gmail.com>
2026-03-11 10:25:30 -0300

    chore: Actualizar Supabase anon key

--- cuidar-frontend/src/app/core/supabase.client.ts
+++ cuidar-frontend/src/app/core/supabase.client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   'https://cvakzhgrnarlcvixhqzx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2YWt6aGdybmFybGN2aXhocXp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMTA1NjIsImV4cCI6MjA1NTc4NjU2Mn0.hHcPDPBn4HBpkSAmRGaRavlOcaTSp7FiVBPN4HBjPDY',
+  'sb_publishable_oFJObocsinXhow22T99Ocg_lZvTrKeq',
   {
     auth: {
       detectSessionInUrl: true,
